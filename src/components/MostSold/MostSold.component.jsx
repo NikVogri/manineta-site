e267b136e6fdc673b80008473b2a30b039f2bfd6
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-02-03 20:42:07 +0100

    added subtext to button

--- src/components/MostSold/MostSold.component.jsx
+++ src/components/MostSold/MostSold.component.jsx
@@ -7,7 +7,11 @@ const MostSold = () => {
   return (
     <section className={styles.mostSold}>
       <Container>
-        <Title title="najbolj" subtitle="prodano" />
+        <Title
+          title="najbolj"
+          subtitle="prodano"
+          paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mauris arcu, maximus eu convallis sit amet."
+        />
         <Button to="/izdelki">Še več</Button>
       </Container>
     </section>
--- src/components/Title/Title.component.jsx
+++ src/components/Title/Title.component.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import styles from "./Title.module.scss"
-const Title = ({ title, subtitle }) => {
+const Title = ({ title, subtitle, paragraph }) => {
   return (
     <h4 className={styles.title}>
       <span className={styles.mainColor}>{title} </span>
       <span>{subtitle}</span>
+      {paragraph ? <p className={styles.paragraph}>{paragraph}</p> : null}
     </h4>
   )
 }
