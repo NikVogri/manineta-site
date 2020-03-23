37517f9bcbd7e626a51dbfd269a906a7076e8f8b
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-03-23 23:05:59 +0100

    removed log

--- src/components/SimilarContent/SimilarContent.component.jsx
+++ src/components/SimilarContent/SimilarContent.component.jsx
@@ -3,7 +3,6 @@ import styles from "./SimilarContent.module.scss"
 import ItemCard from "../ItemCard/ItemCard.component"
 
 const SimilarContent = ({ data }) => {
-  console.log(data)
   return (
     <section className={styles.similarContent}>
       <div className={styles.cardContainer}>
